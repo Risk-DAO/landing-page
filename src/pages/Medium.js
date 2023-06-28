ceb07ba0b9b792b620f682f0d40b55a7adadbc81
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-06-28 18:07:28 +0200

    fading buttons

--- src/pages/Medium.js
+++ src/pages/Medium.js
@@ -1,18 +1,18 @@
+import { useEffect, useState } from "react";
+
 import arrow from "../images/arrow.png";
 import axios from "axios";
-import { useEffect } from "react";
-import { useState } from "react";
 
 const mediumRSSFeedURL = "https://riskdao-landing-api.la-tribu.xyz/medium"
 const styles = {
-    article: {display: 'flex', flexDirection: 'column', justifyContent: 'center', maxHeight: '25vh', minHeight: '25vh'},
+    article: {display: 'flex', flexDirection: 'column', justifyContent: 'space-between', maxHeight: '25vh', minHeight: '25vh'},
     a: {width: '30%', margin: '0 10px 0 0'}
 }
 
 function Card(props) {
-    return (<a style={styles.a} href={props.data.link} target="blank">
-        <article style={styles.article}>
-            <img style={{height:'40%', marginBottom:'5%'}} src={props.data.thumbnail} alt="" />
+    return (<a className="articles-content" style={styles.a} href={props.data.link} target="blank">
+        <article className='fade-in' style={styles.article}>
+            <img style={{height:'10vh', marginBottom:'5%'}} src={props.data.thumbnail} alt="" />
             <small style={{height:'40%', marginTop: '5%'}}>{props.data.title}</small>
         </article>
     </a>
@@ -48,8 +48,8 @@ export default function Medium(props) {
     }
 
 
-    return (<div style={{display: 'flex', justifyContent: 'center', width:'100%', alignItems:'center'}}>
-        <img className={className} src={arrow} alt="left-button" onClick={() => handleButton('left')}/>
+    return (<div style={{display: 'flex', justifyContent: 'center', width:'100%', minHeight:'20vh', alignItems:'center'}}>
+        <img className={className} style={postIndex === 0 ? { opacity:'5%'} : {}} src={arrow} alt="left-button" onClick={() => handleButton('left')}/>
         {mediumData ?
             <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', alignItems:'center'}}>
                 <Card data={mediumData[mediumData.length - 1]}/> 
@@ -57,6 +57,6 @@ export default function Medium(props) {
                 <Card data={mediumData[postIndex + 1]} />
             </div>
             : 'Could not load Medium posts'}
-        <img className={className} style={{transform: 'rotate(180deg'}} src={arrow} alt="right-button" onClick={() => handleButton('right')}/>
+        <img className={className} style={postIndex === mediumData.length - 3 ? { opacity:'5%', transform: 'rotate(180deg'} : {transform: 'rotate(180deg'}} src={arrow} alt="right-button" onClick={() => handleButton('right')}/>
     </div>)
 }
