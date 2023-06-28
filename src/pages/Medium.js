c776c1e570dc546a2d921fe2e48469bfde9b29ab
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-06-28 18:24:47 +0200

    fix

--- src/pages/Medium.js
+++ src/pages/Medium.js
@@ -5,15 +5,15 @@ import axios from "axios";
 
 const mediumRSSFeedURL = "https://riskdao-landing-api.la-tribu.xyz/medium"
 const styles = {
-    article: {display: 'flex', flexDirection: 'column', justifyContent: 'space-between', maxHeight: '25vh', minHeight: '25vh'},
-    a: {width: '30%', margin: '0 10px 0 0'}
+    article: { display: 'flex', flexDirection: 'column', justifyContent: 'space-between', maxHeight: '25vh', minHeight: '25vh' },
+    a: { width: '30%', margin: '0 10px 0 0' }
 }
 
 function Card(props) {
     return (<a className="articles-content" style={styles.a} href={props.data.link} target="blank">
         <article className='fade-in' style={styles.article}>
-            <img style={{height:'10vh', marginBottom:'5%'}} src={props.data.thumbnail} alt="" />
-            <small style={{height:'40%', marginTop: '5%'}}>{props.data.title}</small>
+            <img style={{ height: '10vh', marginBottom: '5%' }} src={props.data.thumbnail} alt="" />
+            <small style={{ height: '40%', marginTop: '5%' }}>{props.data.title}</small>
         </article>
     </a>
     )
@@ -34,29 +34,34 @@ export default function Medium(props) {
         fetchPosts();
     }, []);
 
-    function handleButton(direction){
-        if(direction === 'left'){
-            if(postIndex > 0){
+    function handleButton(direction) {
+        if (direction === 'left') {
+            if (postIndex > 0) {
                 setPostIndex(postIndex - 1);
             }
         }
-        if(direction === 'right'){
-            if(postIndex < (mediumData.length - 3)){
+        if (direction === 'right') {
+            if (postIndex < (mediumData.length - 3)) {
                 setPostIndex(postIndex + 1);
             }
         }
     }
+    if (!mediumData) {
+        return (
+            <div>Could not load Medium posts</div>
+        )
+    }
 
 
-    return (<div style={{display: 'flex', justifyContent: 'center', width:'100%', minHeight:'20vh', alignItems:'center'}}>
-        <img className={className} style={postIndex === 0 ? { opacity:'5%'} : {}} src={arrow} alt="left-button" onClick={() => handleButton('left')}/>
+    return (<div style={{ display: 'flex', justifyContent: 'center', width: '100%', minHeight: '20vh', alignItems: 'center' }}>
+        <img className={className} style={postIndex === 0 ? { opacity: '5%' } : {}} src={arrow} alt="left-button" onClick={() => handleButton('left')} />
         {mediumData ?
-            <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', alignItems:'center'}}>
-                <Card data={mediumData[mediumData.length - 1]}/> 
+            <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <Card data={mediumData[mediumData.length - 1]} />
                 <Card data={mediumData[postIndex]} />
                 <Card data={mediumData[postIndex + 1]} />
             </div>
             : 'Could not load Medium posts'}
-        <img className={className} style={postIndex === mediumData.length - 3 ? { opacity:'5%', transform: 'rotate(180deg'} : {transform: 'rotate(180deg'}} src={arrow} alt="right-button" onClick={() => handleButton('right')}/>
+        <img className={className} style={postIndex === mediumData.length - 3 ? { opacity: '5%', transform: 'rotate(180deg' } : { transform: 'rotate(180deg' }} src={arrow} alt="right-button" onClick={() => handleButton('right')} />
     </div>)
 }
