2d03eaa5bd8fa2db26dc07f4df3a4daad53cae1e
0xVorian <33686588+0xVorian@users.noreply.github.com>
2023-06-29 15:48:14 +0200

    before overall

--- src/pages/Medium.js
+++ src/pages/Medium.js
@@ -1,22 +1,22 @@
-import { useEffect, useState } from "react";
+import { Collapse, List } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 
+import { TransitionGroup } from "react-transition-group";
 import arrow from "../images/arrow.png";
 import axios from "axios";
 
 const mediumRSSFeedURL = "https://riskdao-landing-api.la-tribu.xyz/medium"
-const styles = {
-    article: { display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '25vh' },
-    a: { width:'30%', height:'35vh', margin: '0 1% 0 1%' }
-}
 
-function Card(props) {
-    return (<a style={styles.a} href={props.data.link} target="blank">
-        <article style={styles.article}>
+function renderCard(article, solo=false) {
+    const width = solo ? '33%' : '50%'
+    return (
+        <a style={{ width:width, height:'35vh', margin: '0 1% 0 1%' }} href={article.link} target="blank">
+        <article style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', minHeight: '25vh' }}>
             <header style={{height: '14vh', marginBottom: '0', display:'flex', alignItems:'center ', justifyContent:'center'}}>
-            <img style={{maxHeight:'11vh', marginBottom: '5%' }} src={props.data.thumbnail} alt="" />
+            <img style={{maxHeight:'11vh', marginBottom: '5%' }} src={article.thumbnail} alt="" />
             </header>
             <body style={{ height: '8vh', marginBottom: '5%' }}>
-            <small className="Medium-text-overflow" style={{marginTop: '5%'}}>{props.data.title}</small>
+            <small className="Medium-text-overflow" style={{marginTop: '5%'}}>{article.title}</small>
             </body>
         </article>
     </a>
@@ -26,7 +26,7 @@ function Card(props) {
 export default function Medium(props) {
     const [mediumData, setMediumData] = useState(undefined);
     const [postIndex, setPostIndex] = useState(0);
-
+    const containerRef = useRef();
     const blackMode = props.blackMode;
     const className = blackMode ? 'Medium-buttons-dark' : 'Medium-buttons'
 
@@ -61,17 +61,16 @@ export default function Medium(props) {
             <div>Could not load Medium posts</div>
         )
     }
-
-
-    return (<div style={{ display: 'flex', justifyContent: 'center', width: '100%', minHeight: '20vh', alignItems: 'center' }}>
+    return (<div style={{ display: 'flex', justifyContent: 'center', width: '100%', minHeight: '20vh', alignItems: 'center' }} ref={containerRef}>
         <img className={className} style={postIndex === 0 ? { opacity: '5%' } : {}} src={arrow} alt="left-button" onClick={() => handleButton('left')} />
-        {mediumData ?
-            <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width:'100%' }}>
-                <Card data={mediumData[mediumData.length - 1]} />
-                <Card data={mediumData[postIndex]} />
-                <Card data={mediumData[postIndex + 1]} />
+            <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center', width:'100%' }}>
+                <List style={{ display: "flex", flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width:'100%' }}>
+                <TransitionGroup style={{display: 'flex', flexDirection:'row', justifyContent: 'center', width:'100%' }}>
+                <Collapse>{renderCard((mediumData[mediumData.length - 1]))}</Collapse>
+                {mediumData.filter((_, index) =>(index === postIndex || index === postIndex +1)).map(_ => <Collapse key={_.title}>{renderCard(_)}</Collapse>)}
+                </TransitionGroup>
+                </List>
             </div>
-            : 'Could not load Medium posts'}
         <img className={className} style={postIndex === mediumData.length - 3 ? { opacity: '5%', transform: 'rotate(180deg' } : { transform: 'rotate(180deg' }} src={arrow} alt="right-button" onClick={() => handleButton('right')} />
     </div>)
 }
